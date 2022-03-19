206077f029894a9562e7990fc2d5320bb1b60cfb
howlingunit <93931085+howlingunit@users.noreply.github.com>
2022-03-19 14:47:57 +0000

    api

--- svr.js
+++ svr.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import * as sql from './lib/sql.mjs';
+import * as api from './lib/api.mjs';
+// import * as sql from './lib/sql.mjs';
 
 
 const app = express();
 
 app.use(express.static('static'));
+
+app.get('/getTopics', api.getTopics);
+
+app.get('/getTopic', api.getTopic);
+
+app.post('/addTopic', express.json(), api.addTopic);
+
+app.post('/delTopic', express.json(), api.delTopic);
 
 app.listen(8081);
